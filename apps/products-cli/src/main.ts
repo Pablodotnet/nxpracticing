5dbb7c1010deb6bb929fb18b22ebf4563d1c083b
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2022-12-05 22:19:06 -0800

    Commiting to test affected

---
+++ apps/products-cli/src/main.ts
@@ -0,0 +1,19 @@
+import { createProductsDataClient, ProductsDataClient } from '@my-products/products-data-client'
+
+main()
+
+async function main() {
+    const productsDataClient: ProductsDataClient = createProductsDataClient()
+    const id: string = getProvidedId()
+    if (id != null) {
+        const product = await productsDataClient.getProductById(id)
+        console.log(JSON.stringify(product, null, 2))
+    } else {
+        const products = await productsDataClient.getProducts()
+        console.log(JSON.stringify(products, null, 2))
+    }
+}
+
+function getProvidedId() {
+    return process.argv[2]
+}
